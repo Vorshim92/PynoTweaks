e4c20c5bd11bfda7a97baddd93cb1c1d5b9ee86f
ioshiro <dmitry.golyshev.dev@gmail.com>
2023-05-06 01:10:29 +0200

    add killyno command

---
+++ media/lua/server/PynoTweaksComands.lua
@@ -0,0 +1,15 @@
+function killyno(player, args)
+    if not isServerAdmin(player) then return end
+    local name = args[1]
+    local target = getPlayerByUserNameForCommand(name)
+    local kills = args[2]
+    local lifeTime = args[3]
+    target:setZombieKills(kills)
+    target:setHoursSurvived(lifeTime)
+end
+
+function registerkillyno()
+    registerCommand("killyno", killyno)
+end
+
+Events.OnGameStart.Add(registerkillyno)
