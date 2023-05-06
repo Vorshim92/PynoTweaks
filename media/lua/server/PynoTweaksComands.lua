c8001123b6c95713aeeabf2592e47b49b2dc8c0a
ioshiro <dmitry.golyshev.dev@gmail.com>
2023-05-06 19:42:47 +0200

    update comand to lua_server_commands

--- media/lua/server/PynoTweaksComands.lua
+++ media/lua/server/PynoTweaksComands.lua
@@ -1,21 +1,26 @@
-require("CommandsManager")
+require 'lua_server_commands'
 
-function killyno(player, args)
-    if not isAdmin() and not getPlayer():getAccessLevel() ~= "None" then return end
-    local name = args[1]
-    local target = getPlayerByUserNameForCommand(name)
-    local kills = args[2]
-    local lifeTime = args[3]
-    target:setZombieKills(kills)
-    target:setHoursSurvived(lifeTime)
-end
+LuaServerCommands.register('killyno', function(author, command, args)
+--not isAdmin() and
+    if  not author:getAccessLevel() ~= "None" then return 'usable only by adminos' end
+    -- Check if the correct number of arguments are passed.
+    if #args ~= 2 then
+        return '/killyno [kills] [hours]'
+    end
 
-function registerkillyno()
-    registerCommand("killyno", killyno)
-end
+    -- NOTE: The helper only becomes visible in global scope when the first lua server command is fired.
+    --       Make sure to reference the helper inside of the command's handler function.
+    local helper = LuaServerCommandHelper
 
-function registerCommand(commandName, commandFunction)
-    Commands[commandName] = commandFunction
-  end
-  
-Events.OnGameStart.Add(registerkillyno)
+    -- Attempt to resolve the player using the helper method.
+    local username = args[1]
+    local player = helper.getPlayerByUsername(username)
+    if player == nil then return 'Player not found: '..tostring(username) end
+    local kills = tonumber(args[2])
+    local lifeTime = tonumber(args[3])
+    player:setZombieKills(kills)
+    player:setHoursSurvived(lifeTime)
+    player:Say("Daghe")
+
+    return 'Kills and Survived time setted!'
+end)
