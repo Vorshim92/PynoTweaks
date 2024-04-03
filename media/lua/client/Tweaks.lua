b13366ec19c1cd99952712925d07e36afa78b123
pynopyno <magnanimog@gmail.com>
2024-04-03 17:45:42 +0200

    cambiato il trapano OP

--- media/lua/client/Tweaks.lua
+++ media/lua/client/Tweaks.lua
@@ -449,10 +449,17 @@ TweakItem("Base.Bag_ALICEpack_Pink","Capacity", 27);
 TweakItem("Base.Battery","Weight", 0.02);
 
 
--- Lancia fumogena
-TweakItem("Base.SmokeBomb","SmokeRange", 50);
+-- bomba fumogena
 TweakItem("Base.SmokeBomb","Weight", 0.3);
-TweakItem("Base.SmokeBomb","MaxDamage", 50.0);
+
+-- Trapano La Resistenza Pack
+TweakItem("Base.CordlessDrill","Weight", 1.0);
+TweakItem("Base.CordlessDrill","MaxDamage", 0.9);
+TweakItem("Base.CordlessDrill","MinDamage", 0.3);
+TweakItem("Base.CordlessDrill","MinDamage", 0.3);
+TweakItem("Base.CordlessDrill","ConditionMax", 12);
+
+
 
 
 
