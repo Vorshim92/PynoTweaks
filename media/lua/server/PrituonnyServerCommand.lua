165250a85eb2587c83bcf8be8a15c14c8b2067bd
Stefano Scalfari <vorshim92@gmail.com>
2025-01-21 01:11:25 +0100

    fix prituonny + premium edition xD

--- media/lua/server/PrituonnyServerCommand.lua
+++ media/lua/server/PrituonnyServerCommand.lua
@@ -1,21 +1,36 @@
+if isClient() then return end
 local Commands = {}
 function Commands.onSendPrituonny(player, args)
     local clickedPlayer = getPlayerByOnlineID(args.player)
-    local prituonnyList = ModData.getOrCreate("Prituonny")
-    prituonnyList[clickedPlayer:getUsername()] = true
-    ModData.add("Prituonny", prituonnyList)
+    ModData.getOrCreate("Prituonny")[clickedPlayer:getUsername()] = {super = false}
     ModData.transmit("Prituonny")
     sendServerCommand(clickedPlayer, "Prituonny", "onSendPrituonny", args)
 end
 
 function Commands.onRemovePrituonny(player, args)
     local clickedPlayer = getPlayerByOnlineID(args.player)
-    local prituonnyList = ModData.getOrCreate("Prituonny")
-    prituonnyList[clickedPlayer:getUsername()] = false
-    ModData.add("Prituonny", prituonnyList)
+    local PriTuonnyMod = ModData.getOrCreate("Prituonny")
+    if PriTuonnyMod[clickedPlayer:getUsername()].super == true then
+        sendServerCommand(clickedPlayer, "Prituonny", "onRemoveSuperPrituonny", args)
+    end
+    PriTuonnyMod[clickedPlayer:getUsername()] = nil
     ModData.transmit("Prituonny")
     sendServerCommand(clickedPlayer, "Prituonny", "onRemovePrituonny", args)
 end
+function Commands.onSuperPrituonny(player, args)
+    local clickedPlayer = getPlayerByOnlineID(args.player)
+    ModData.getOrCreate("Prituonny")[clickedPlayer:getUsername()] = {super = true}
+    ModData.transmit("Prituonny")
+    sendServerCommand(clickedPlayer, "Prituonny", "onSuperPrituonny", args)
+end
+function Commands.onRemoveSuperPrituonny(player, args)
+    local clickedPlayer = getPlayerByOnlineID(args.player)
+    ModData.getOrCreate("Prituonny")[clickedPlayer:getUsername()] = {super = false}
+    ModData.transmit("Prituonny")
+    sendServerCommand(clickedPlayer, "Prituonny", "onRemoveSuperPrituonny", args)
+end
+
+
 
 Events.OnClientCommand.Add(function(module, command, player, args)
 	if module == 'Prituonny' and Commands[command] then
